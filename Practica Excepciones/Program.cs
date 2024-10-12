83905541be9551360e5861ff37adbc33d3ad8e1d
Reidy07 <reidybrown1234@gmail.com>
2024-10-12 18:35:46 -0400

    Agregar archivos de proyecto.

---
+++ Practica Excepciones/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace PracticaExcepciones
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+                Console.WriteLine("Ingrese su nombre");
+                string nombre = Console.ReadLine();
+
+
+                Console.WriteLine("Ahora ingrse la contraseña");
+                string contraseña = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(nombre))
+                {
+                    throw new ArgumentException("El nombre no debe de estar vacio");
+                }
+                if (contraseña.Length < 8)
+                {
+                    throw new ArgumentException("La contraseña debe de tener mas de 8 digitos");
+                }
+                Console.WriteLine("LA validacion se hizo exitosamente");
+            }
+            catch (ArgumentException ex)
+			{
+                Console.WriteLine($"Error: {ex.Message}");
+			}
+            finally
+            {
+                Console.WriteLine("El proceso ya a finalizado");
+            }
+
+        }   
+
+    }
+}
